e3502690dce74d1b408bade26d81baaea5a4a53d
Ramz10o <134755948+Ramz10o@users.noreply.github.com>
2024-08-30 22:03:44 +0530

    TicTacToe in java

--- Simple Programs/Java/TiicTacToe.java
+++ Simple Programs/Java/TiicTacToe.java
@@ -1,3 +1,9 @@
+/*
+The following code is to run a simple TicTacToe game in your terminal with java language. 
+The following code creates a game for two persons with their names.
+The winning person name is displayed as won if any of the players won the game otherwise Tie if no one wins.
+*/ 
+
 import java.util.*;
 public class TicTacToe {
     List<String> board;
