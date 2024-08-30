4cce1b71c3a6b4c7737ec50e029cf32ec575e97e
Ramz10o <134755948+Ramz10o@users.noreply.github.com>
2024-08-30 22:03:20 +0530

    TicTacToe in cpp

--- Simple Programs/C++/TicTacToe.cpp
+++ Simple Programs/C++/TicTacToe.cpp
@@ -1,7 +1,7 @@
 /*
 The following code is to run a simple TicTacToe game in your terminal with c++ language. 
 The following code creates a game for two persons with their names.
-The winning person anme is displayed as won if any of the players won the game otherwise Tie if no one wins.
+The winning person name is displayed as won if any of the players won the game otherwise Tie if no one wins.
 */ 
 
 #include <iostream>
