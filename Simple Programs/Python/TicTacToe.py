19b9bfbeec8b4ee0abd5457833d17174e4d80cbd
Ramz10o <134755948+Ramz10o@users.noreply.github.com>
2024-09-01 22:03:54 +0530

    TicTacToe

--- Simple Programs/Python/TicTacToe.py
+++ Simple Programs/Python/TicTacToe.py
@@ -11,17 +11,17 @@ class TicTacToe :
   def __init__(self,Name1,Name2) :
     self.Name1,self.turn = Name1,' X '
     self.Name2,self.flag = Name2,0
-    self.Curr = self.Name1
-    self.Turn = {self.Name1 : ' X ', self.Name2 : ' O '}
+    self.current = self.Name1
+    self.turn = {self.Name1 : ' X ', self.Name2 : ' O '}
     self.board=["   " for x in range(9)]
     self.winPatterns=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
     
   def __makeMove(self):
-    print(f'{self.Curr} Turn')
+    print(f'{self.current} turn')
     pos=(int(input(f"Enter Row : "))-1)*3+(int(input("Enter column : "))-1)
     if pos<0 or pos>8 or self.board[pos]!='   ' :
         return 1
-    self.board[pos]=self.turn[self.Curr]
+    self.board[pos]=self.turn[self.current]
     
   def __printGame(self):
     print(f'''
@@ -40,7 +40,7 @@ class TicTacToe :
     
   def __checkWin(self):
     for i in self.winPatterns:
-      p = self.turn[self.Curr]
+      p = self.turn[self.current]
       if self.board[i[0]]==self.board[i[1]] and self.board[i[0]]==self.board[i[2]] and self.board[i[1]]==p :
         return True
     return False
@@ -52,10 +52,10 @@ class TicTacToe :
           continue
       self.__printGame()
       if(self.__checkWin()):
-        print(f'\n{self.Curr} Wins !')
+        print(f'\n{self.current} Wins !')
         self.flag = 1
         break
-      self.Curr=self.Name1 if self.Curr==self.Name2 else self.Name2
+      self.current=self.Name1 if self.current==self.Name2 else self.Name2
     if(self.flag==0 and '   ' not in self.board):
       print('\nTie !')
       
