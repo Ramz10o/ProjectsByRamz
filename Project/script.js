6ea4cd3d1850eea6c00744aaa27b67d1f8682f1e
Ramz10o <134755948+Ramz10o@users.noreply.github.com>
2024-10-14 21:15:18 +0530

    Small changes

--- Project/script.js
+++ Project/script.js
@@ -11,7 +11,7 @@ async function signout(){
         if (response.ok) {
             alert(msg.message);
             localStorage.removeItem('email');
-            window.location.href = './Login_SignUp.html';
+            window.location.href = './Login.html';
         } else {
             window.alert('Error logging out : ' + msg.message);
         }
@@ -33,34 +33,10 @@ async function valid(){
             const msg = await response.json();
             if (! response.ok){
                 alert(msg.message);
-                window.location.href = './Login_SignUp.html';
+                window.location.href = './Login.html';
             }
         } catch (err) {
             console.error(err);
             alert('Error loading page');
           }
-    }
-    
-async function validHome(){
-            try {
-                const email = localStorage.getItem('email');
-                const response = await fetch('http://localhost:5000/valid', {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify({ email: email }),
-                });
-        
-                const msg = await response.json();
-                if ( response.ok ){
-                    if(! (msg.message === 'Done')){
-                        alert(msg.message);   
-                    }         
-                }else {
-                    alert(msg.message);
-                    window.location.href = './Login_SignUp.html';
-                }
-            } catch (err) {
-                console.error(err);
-                alert('Error loading page');
-              }
     }
