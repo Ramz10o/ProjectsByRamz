4e4b58d6fb7d1b88df294f9136ea3918e4808cb0
Ramz10o <134755948+Ramz10o@users.noreply.github.com>
2024-10-23 10:35:07 +0530

    Done

--- person_management_system_php/db_connection.php
+++ person_management_system_php/db_connection.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost';
-$user: "u739593535_host";
-$password: "^3GqRLE8rW";
-$database: "u739593535_personDatabase";
+$user = "u739593535_host";
+$password = "^3GqRLE8rW";
+$database = "u739593535_personDatabase";
 
 $conn = new mysqli($host, $user, $password, $database);
 
