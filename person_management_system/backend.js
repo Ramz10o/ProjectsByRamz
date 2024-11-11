c65ea870e794d70f7a7ef6bfc5b71dc59c5a8c15
Ramz10o <134755948+Ramz10o@users.noreply.github.com>
2024-11-11 20:01:40 +0530

    Chat dhi max ipoyindi

--- person_management_system/backend.js
+++ person_management_system/backend.js
@@ -2,174 +2,139 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const bodyParser = require('body-parser');
+const { Server } = require("socket.io");
+const http = require('http');
 const multer = require('multer');
 const cors = require('cors');
-const { Records, Login } = require('./models/models');
+const { Users, Logins, Sockets, Chats, Leaderboard } = require('./models');
 
 const app = express();
-const PORT = 5000;
-const URI = "mongodb+srv://personmanagementsystem:Pa85kZ8tWUfQ7Bvi@cluster0.h5bxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-const logout = (async () => {
-    if(Login.findOne({isLoggedIn : true})){
-        await Login.findOneAndUpdate({isLoggedIn : true},{isLoggedIn : false});
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true 
     }
 });
-const login = (async () => {
-    if(Login.findOne({isLoggedIn : false})){
-        await Login.findOneAndUpdate({isLoggedIn : false},{isLoggedIn : true});
-    }
-}); 
+const PORT = 5000;
+const URI = 'mongodb+srv://ramz:bM0cFn3cdNa7yLfE@cluster0.eupad.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('uploads'));
+app.use(express.static('Web Application Suite'));
 
-mongoose.connect(URI, { dbName : 'UserData' }, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(URI, { dbName : 'SuiteDataBase' }, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error(err));
 
 app.post('/login', async (req, res) => {
-    const { email, password } = req.body;
-    try {
-        console.log('Login attempt');
-        const user = await Login.findOne({ email : email });
-        if (!user) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: 'Invalid credentials' });
-        }
-        login();
-        console.log('Login Successful');
-        return res.status(200).json({ message : 'Login Successful'});
-    } catch (error) {
-        console.error(error)
-        return res.status(500).json({ message: 'Server error' });
-    }
-});
-
-app.post('/addRecord', upload.single('pic'), async (req,res) => {
-    const {name, dob, email, phone, user} = req.body;
-    try{
-        console.log('Attempt to add record');
-        if(!req.file.mimetype.startsWith('image/')){
-            return res.status(400).json({ message : 'Only image files are allowed ' });
-        }
-        if(req.file.size > 1024 * 1024){
-            return res.status(400).json({ message : 'Image size should be less than 1 MB' });
-        }
-        const record1 = await Records.findOne({ email : email });  
-        if(record1){
-            return res.status(400).json({ message : 'Email already exists with different person' });
-        }
-        const record2 = await Records.findOne({ phone : phone });  
-        if(record2){
-            return res.status(400).json({ message : 'Phone already exists with different person' });
-        }
-        const details = new Records({ username : user, name : name, dob :  dob, email : email, phone : phone, 
-            pic : {
-                data : Buffer.from(req.file.buffer).toString('base64'),
-                extension : req.file.mimetype
-            }});
-        await details.save();
-        console.log('Insertion successful');
-        return res.status(200).json({ message : 'Insertion successful' });
-    }
-    catch(error){
-        console.error(error);
-        return res.status(500).json({ message : 'Server error' });
-    }
-});
-
-app.post('/getRecords', async (req,res) => {
-    try{
-        console.log('Retrieval attempt');
-        const records = await Records.find({}).sort(req.body.field);
-        if(! records.length){
-            console.log('Empty');
-            return res.status(404).json({ message : 'Empty' });
-        }
-        console.log('Retrieval successful');
-        return res.status(200).json({ records : records, message : 'Found' });
-    }
-    catch(error){
-        console.error(error);
-        return res.status(500).json({ message : 'Server error' });
-    }
-});
-
-app.post('/deleteRecord', async (req,res) => {
-    try{
-        console.log('Deletion initiated');
-        const record = await Records.findOne({ email : req.body.delid });
-        if(! record){
-            return res.status(404).json({ message : 'Record not found' });
-        }
-        await Records.findOneAndDelete({ email : req.body.delid });
-        console.log('Record deleted successfully');
-        return res.status(200).json({ message : 'Record deleted successfully' });
-    }
-    catch(error){
-        console.error(error);
-        return res.status(500).json({ message : 'Server error' });
-    }
-});
-
-app.post('/logout', async (req, res) => {
-        try{
-            console.log('Logout attempt')
-            const user = await Login.findOne({ isLoggedIn : true });
+        const { email, password } = req.body;
+        try {
+            console.log('Login attempt');
+            const user = await Users.findOne({ email : email });
             if (!user) {
-                return res.status(400).json({ message : 'No login found' });
+                return res.status(400).json({ message: 'Invalid credentials' });
             }
-            logout();
-            console.log(`Logout Successful`); 
-            return res.status(200).json({ message: 'Logout successful' });
+            const isMatch = await bcrypt.compare(password, user.password);
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Invalid credentials' });
+            }
+            const Login = new Logins({
+                user: user._id
+            });
+            await Login.save();
+            console.log('Login Successful');
+            return res.status(200).json({user : user._id});
+        } catch (error) {
+            console.error(error)
+            return res.status(500).json({ message: 'Server error' });
         }
-        catch (error){
+});
+
+app.post('/signup', async (req,res) => {
+    const { email, password, username } = req.body;
+    try{
+        const existingUser = await Users.findOne({ username : username.toLowerCase() });
+        if(existingUser){
+            return res.status(400).json({ message: 'User Name already in use' });
+        }
+        const existingEmail = await Users.findOne({ email : email });
+        if(existingEmail){
+            return res.status(400).json({ message: 'Email already in use' });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new Users({
+            email: email,
+            password: hashedPassword,
+            username: username.toLowerCase()
+        });
+        await user.save();
+        console.log('User created');
+        return res.status(200).json({});
+        } 
+        catch (error) {
             console.error(error);
             return res.status(500).json({ message: 'Server error' });
         }
-    });
-
-app.post('/find', async (req,res) => {
-        try{
-            console.log('Find initiated');
-            const query = req.body.query;
-            const records = await Records.find({ $or: 
-                [ {name : query}, {email : query}, {phone : query}, {dob : query} ] 
-            });
-            if(! records.length){
-                console.log('Not Found');
-                return res.status(404).json({ message : 'No Records found' });
-            }
-            console.log('find successful');
-            return res.status(200).json({ message : 'Found', records : records });
-        }
-        catch(error){
-            console.error(error);
-            return res.status(500).json({ message : 'Server error' });
-        }
-    });
-
-app.post('/valid', async (req,res) =>{
-    try{
-        if(! await Login.findOne({isLoggedIn : true})){
-            return res.status(400).json({});
-        }
-        return res.status(200).json({});
-    }
-    catch(error){
-        console.error(error);
-        return res.status(500).json({ message : 'Server error' });
-    }
 });
 
-logout();
+io.on('connection', (socket) => {
+    console.log('Client connected',socket.id);
+    socket.on('details', async (data) => {
+        const existing = Sockets.findOne({user : data.user});
+        if(existing){
+            await Sockets.findOneAndUpdate({user : data.user},{ socket : socket.id});
+        }
+        else{
+            const sock = new Sockets({
+                socket: socket.id,
+                user: data.user,
+                app: data.app
+              });
+              await sock.save();
+        }
+          const activeUsers = Sockets.find({app : data.app});
+          socket.broadcast.emit('Online',activeUsers);
+          socket.to(socket.id).emit('Online',activeUsers);
+    });
+    socket.on('send_message', async (msg) => {
+        const to = Users.findOne({ username : msg.to });
+        const chat = new Chats({
+            from: msg.from,
+            to: to._id,
+            message: msg.type === 'text' ? msg.message : Buffer.from(msg.message).toString('base64'),
+            type: msg.type
+        });
+        await chat.save();
+        const chats = Chats.find( {$or : [{ from : msg.from, to : to._id},{from : to._id, to : msg.from}] }).sort('time');
+        socket.to(socket.id).emit('get_message', chats);
+        const toSocket = await Sockets.findOne({ user:to._id , app:'Chatzz'});
+        if(toSocket){
+            socket.to(toSocket.socket).emit('get_message', chats);
+        }
+    });
+    socket.on('get_message', async (msg) => {
+        const to = Users.findOne({ username : msg.to });
+        const chats = Chats.find( {$or : [{ from : msg.from, to : to._id},{from : to._id, to : msg.from}] }).sort('time');
+        socket.to(socket.id).emit('get_message', chats);
+    })
+    socket.on('invite', (from) => {
+    });
+    socket.on('accept', () => {
+    });
+    socket.on('reject', () => {
+    });
+    socket.on('play', (game) => {
+    });
+    socket.on('disconnect', () => {
+        Sockets.findOneAndDelete({ socket:socket.id});
+        console.log('Client disconnected');
+    });
+});
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
